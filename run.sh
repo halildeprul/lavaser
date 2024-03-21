28acd4f7a8d7d95750bcab21273271754940a486
halildefrul <70564285+halildeprul@users.noreply.github.com>
2024-03-21 20:49:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-94f7381e-9841-43a2-b4a8-19ea9f5ced46/)
 
 
 echo "$curl_output" >> Update.tsx
